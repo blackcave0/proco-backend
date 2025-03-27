b7007b152cd96a0c708768a1d6cbc0702e761a03
blackcave0 <i3intel@protonmail.com>
2025-03-27 14:15:39 +0530

    Update CORS configuration to allow requests from any origin and add MongoDB URI to environment variables

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://proco-eight.vercel.app", // Replace with your frontend URL
+    // origin: "https://proco-eight.vercel.app", // Replace with your frontend URL
+    origin: "*", // Replace with your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
